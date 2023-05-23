f2408333207edf83f5c6dde140b15d9adb698342
Pokye <98105181+Pokye@users.noreply.github.com>
2023-05-23 14:45:47 -0300

    Inicio

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const body = document.querySelector('body'),
+	sidebar = body.querySelector('nav'),
+	toggle = body.querySelector(".toggle"),
+	modeSwitch = body.querySelector(".toggle-switch"),
+	modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click", () => {
+	sidebar.classList.toggle("close");
+})
+
+modeSwitch.addEventListener("click", () => {
+	body.classList.toggle("dark");
+
+	if (body.classList.contains("dark")) {
+		modeText.innerText = "Claro";
+	} else {
+		modeText.innerText = "Escuro";
+
+	}
+});
